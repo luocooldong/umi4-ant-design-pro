2e26039c79650a290820512cda0ac4c34c95b894
gengyudong02 <gengyudong02@58.com>
2022-06-19 13:52:43 +0800

    feat: init

--- src/pages/dashboard/jxtPage/index.tsx
+++ src/pages/dashboard/jxtPage/index.tsx
@@ -11,54 +11,38 @@ import { JxtSelect, JxtSearchSelect } from 'jxt-components'
 const { Item } = Form
 
 const JxtTest = () => {
-  const [form] = Form.useForm()
+    const [form] = Form.useForm()
 
-  // 培训车型
-  const [carTypeList, setCarTypeList] = useState<ComboBox[]>([])
+    // 培训车型
+    const [carTypeList, setCarTypeList] = useState<ComboBox[]>([])
 
-  useEffect(() => {
-    getComboBox('/api/combobox/enum', { enumName: 'cartype' }).then(res => {
-      setCarTypeList(res)
-    })
-  }, [])
+    useEffect(() => {
+        getComboBox('/api/combobox/enum', { enumName: 'cartype' }).then(res => {
+            setCarTypeList(res)
+        })
+    }, [])
 
-  return (
-    <div>
-      <Row>
-        <div style={{ marginBottom: 30 }}>test-lib</div>
-      </Row>
-      <Row gutter={10}>
-        <Form
-          form={form}
-          autoComplete='off'
-          colon={false}
-          layout='inline'
-          style={{ width: '100%' }}
-          onFinish={e => {}}
-        >
-          <Col span={3}>
-            <Item name='name'>
-              <JxtSearchSelect
-                api='/api/combobox/getCarList'
-                placeholder='车牌号'
-                key='name'
-              />
-            </Item>
-          </Col>
-          <Col span={3}>
-            <Item name='carType'>
-              <JxtSelect
-                key='carType'
-                optionDisable={3}
-                list={carTypeList}
-                placeholder='培训车型'
-              />
-            </Item>
-          </Col>
-        </Form>
-      </Row>
-    </div>
-  )
+    return (
+        <div>
+            <Row>
+                <div style={{ marginBottom: 30 }}>test-lib</div>
+            </Row>
+            <Row gutter={10}>
+                <Form form={form} autoComplete='off' colon={false} layout='inline' style={{ width: '100%' }} onFinish={e => {}}>
+                    <Col span={3}>
+                        <Item name='name'>
+                            <JxtSearchSelect api='/api/combobox/getCarList' placeholder='车牌号' key='name' />
+                        </Item>
+                    </Col>
+                    <Col span={3}>
+                        <Item name='carType'>
+                            <JxtSelect key='carType' optionDisable={3} list={carTypeList} placeholder='培训车型' />
+                        </Item>
+                    </Col>
+                </Form>
+            </Row>
+        </div>
+    )
 }
 
 export default JxtTest
